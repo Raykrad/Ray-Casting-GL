93c1145a54ead25b3d330640b74207ddd190e11c
Raykrad <109039576+Raykrad@users.noreply.github.com>
2024-05-14 16:23:58 +0300

    Add files via upload

---
+++ external/install_external.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+git clone https://github.com/g-truc/glm.git
+echo 'GLM'
+
+git clone https://github.com/glfw/glfw.git
+echo 'GLFW'
+
+echo 'Installation is over'
+
---
+++ external/install_win_external.bat
@@ -0,0 +1,7 @@
+git clone https://github.com/g-truc/glm.git
+echo 'GLM'
+
+git clone https://github.com/glfw/glfw.git
+echo 'GLFW'
+
+echo 'Installation is over'
